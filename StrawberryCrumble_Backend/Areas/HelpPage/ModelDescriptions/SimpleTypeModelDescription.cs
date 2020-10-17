1b4d33b9eafc7aa3879eef782bfee1d6b15e5f0b
Sam Young <samwyoung@outlook.com>
2020-10-17 16:21:17 +1100

    Initial proj create

---
+++ StrawberryCrumble_Backend/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace StrawberryCrumble_Backend.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ StrawberryCrumble_Backend/Controllers/StrawberryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace StrawberryCrumble_Backend.Controllers
+{
+    public class StrawberryController : ApiController
+    {
+        // GET: api/Strawberry
+        public JsonResult<string[]> Get()
+        {
+            var xml = new string[] { "Crumbleeeeeeeeeeeee", "value2" };
+            return Json(xml);
+        }
+
+        // GET: api/Strawberry/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Strawberry
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Strawberry/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Strawberry/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
